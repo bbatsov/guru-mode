03ab9d2667b2262ac5ffa4083f55894568da213a
Bozhidar Batsov <bozhidar@batsov.dev>
2021-10-25 14:56:32 +0300

    Extend the commentary

--- guru-mode.el
+++ guru-mode.el
@@ -29,6 +29,10 @@
 ;;; Commentary:
 ;;
 ;; Guru mode teaches you how to use Emacs effectively.
+;; In particular it promotes the use of idiomatic keybindings
+;; for essential editing commands.  It can be configured to
+;; either disallow the alternative keybindings completely or
+;; to warn when they are being used.
 ;;
 ;;; Code:
 
